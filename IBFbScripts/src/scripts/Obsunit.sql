569c632e2051bca0f3d1db1877bdaabb47ed6409
daniel <daniel@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-09 12:16:29 +0000

    for gcp-4062 gcp-3963
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3000 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Obsunit.sql
+++ IBFbScripts/src/scripts/Obsunit.sql
@@ -1,1 +1,16 @@
+delimiter $$
+
+drop procedure if exists `getObsunitList`$$
+
+CREATE PROCEDURE `getObsunitList`()
+begin
+
+select 
+project_id as effect_id, 
+nd_experiment_id as ounitid 
+from nd_experiment_project nep 
+inner join project_relationship pr on pr.subject_project_id = nep.project_id and pr.type_id = 1150 
+order by project_id;
+
+end$$
 
