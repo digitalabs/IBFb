2a7c1c819ff7af5c95998b78272a3fdceeb6796f
daniel <daniel@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-10 04:35:49 +0000

    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3012 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/DataN.sql
+++ IBFbScripts/src/scripts/DataN.sql
@@ -56,22 +56,21 @@ drop procedure if exists `getDataNList`$$
 CREATE PROCEDURE `getDataNList`()
 begin
 
-	select nep.nd_experiment_id as ounitid, label.projectprop_id as variatid, p.value 
-	from phenotype p 
-	inner join nd_experiment_phenotype nep on p.phenotype_id = nep.phenotype_id 
-	inner join nd_experiment_project nexp on nexp.nd_experiment_id = nep.nd_experiment_id 
-	inner join projectprop pp on pp.value = p.observable_id and pp.project_id = nexp.project_id 
-	INNER JOIN projectprop label ON label.project_id = pp.project_id AND label.rank = pp.rank 
-	WHERE pp.type_id = 1070 and label.type_id in(1043, 1048)
-	and exists 
-	( 
-	select 1 
-	from cvterm cvt1 
-	inner join cvterm_relationship cvtr ON cvt1.cvterm_id = cvtr.subject_id 
-	INNER JOIN cvterm cvt3 ON cvtr.object_id = cvt3.cvterm_id 
-	where p.observable_id = cvt1.cvterm_id 
-	and cvt3.cvterm_id = 1110 
-	) 
-	group by ounitid, variatid , p.value;
+	select nep.nd_experiment_id as ounitid, label.projectprop_id as variatid, p.value
+from phenotype p
+inner join projectprop pp on pp.value = observable_id 
+and exists
+(
+select 1
+from cvterm cvt1
+inner join cvterm_relationship cvtr ON cvt1.cvterm_id = cvtr.subject_id
+INNER JOIN cvterm cvt3 ON cvtr.object_id = cvt3.cvterm_id
+where p.observable_id = cvt1.cvterm_id
+and cvt3.cvterm_id = 1110
+)
+INNER JOIN projectprop label ON label.rank = pp.rank and label.project_id = pp.project_id and pp.type_id = 1070 and label.type_id in(1043, 1048)
+inner join nd_experiment_phenotype nep on p.phenotype_id = nep.phenotype_id
+inner join nd_experiment_project nexp on nexp.nd_experiment_id = nep.nd_experiment_id and pp.project_id = nexp.project_id
+group by ounitid, variatid, p.value
 
 end$$
