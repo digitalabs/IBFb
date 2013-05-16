bdb640b6ea12eca74fe2ad473b487f0e5ccb5a48
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-16 11:08:51 +0000

    GCP 4267 getTraitsById
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3134 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Traits.sql
+++ IBFbScripts/src/scripts/Traits.sql
@@ -15,3 +15,23 @@ begin
 			   AND isa.object_id = trg.cvterm_id
 		);
 end$$
+
+drop procedure if exists `getTraitsById`$$
+
+CREATE PROCEDURE `getTraitsById` (IN v_traitid int)
+begin
+	SELECT DISTINCT 
+			cvt.cvterm_id AS tid,
+			cvt.cvterm_id AS traitid,
+			cvt.name AS trname,
+			cvt.definition AS trdesc,
+			1 AS nstat, 
+			grp.name AS traitgroup
+	  FROM cvterm cvt
+	 INNER JOIN cvterm_relationship cvr ON cvr.object_id = cvt.cvterm_id
+	 INNER JOIN cvterm_relationship gcvr ON gcvr.subject_id = cvt.cvterm_id
+	 INNER JOIN cvterm grp ON grp.cvterm_id = gcvr.object_id
+	 WHERE gcvr.type_id = 1225 -- get "is a" relationship to get group name
+	   AND cvr.type_id = 1200 -- get "has property" relationships
+	   AND cvt.cvterm_id = v_traitid;
+end$$
