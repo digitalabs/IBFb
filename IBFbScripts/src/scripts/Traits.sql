e9cc8f716f4ceed3a36eafe511a53de6d3992686
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-16 09:19:33 +0000

    GCP 4271
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3129 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Traits.sql
+++ IBFbScripts/src/scripts/Traits.sql
@@ -1,1 +1,17 @@
+delimiter $$
 
+drop procedure if exists `getTraitGroups`$$
+
+CREATE PROCEDURE `getTraitGroups`()
+begin
+	SELECT DISTINCT trg.name AS traitgroup
+	  FROM cvterm trg
+	 WHERE EXISTS (
+			SELECT NULL
+			  FROM cvterm_relationship tr
+			 INNER JOIN cvterm_relationship isa ON isa.subject_id = tr.object_id
+			 WHERE tr.type_id = 1200 -- get traits
+			   AND isa.type_id = 1225 -- get "is a" to get groupname
+			   AND isa.object_id = trg.cvterm_id
+		);
+end$$
