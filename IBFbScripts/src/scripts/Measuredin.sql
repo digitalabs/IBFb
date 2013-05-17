8681327a1587a0a23da6a21f8f1647e66676a193
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-17 01:32:01 +0000

    GCP 4129 getListMeasuredin
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3147 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Measuredin.sql
+++ IBFbScripts/src/scripts/Measuredin.sql
@@ -1,1 +1,50 @@
+delimiter $$
+drop procedure if exists `getListMeasuredIn`$$
+
+CREATE PROCEDURE `getListMeasuredIn` (
+IN v_measuredinid int,
+IN v_traitid int,
+IN v_tmethid int,
+IN v_scaleid int,
+IN v_standardscale int)
+begin
+	SET @sql := CONCAT("SELECT cvt.cvterm_id AS measuredinid, ",
+				"crp.object_id AS traitid, ",
+				"crm.object_id AS tmethid, ",
+				"crs.object_id AS scaleid, ",
+				"crs.object_id AS standardscale ",
+				"FROM cvterm cvt ",
+				"INNER JOIN cvterm_relationship crp ON crp.subject_id = cvt.cvterm_id ",
+				"INNER JOIN cvterm_relationship crm ON crm.subject_id = cvt.cvterm_id ",
+				"INNER JOIN cvterm_relationship crs ON crs.subject_id = cvt.cvterm_id ",
+				"WHERE cvt.cv_id = 1040 ",
+				"AND crp.type_id = 1200 ",
+				"AND crm.type_id = 1210 ",
+				"AND crs.type_id = 1220 ");
+
+	IF(v_measuredinid IS NOT NULL) THEN
+		SET @sql = CONCAT(@sql," AND cvt.cvterm_id = ",v_measuredinid);
+    END IF;
+
+	IF(v_traitid IS NOT NULL) THEN
+		SET @sql = CONCAT(@sql," AND crp.object_id = ",v_traitid);
+    END IF;
+
+	IF(v_tmethid IS NOT NULL) THEN
+		SET @sql = CONCAT(@sql," AND crm.object_id = ",v_tmethid);
+    END IF;
+
+	IF(v_scaleid IS NOT NULL) THEN
+		SET @sql = CONCAT(@sql," AND crs.object_id = ",v_scaleid);
+    END IF;
+	
+	IF(v_standardscale IS NOT NULL) THEN
+		SET @sql = CONCAT(@sql," AND crs.object_id = ",v_standardscale);
+    END IF;
+
+	SET @sql = CONCAT(@sql, " ORDER BY measuredinid; ");
+	
+	PREPARE stmt FROM @sql;
+	EXECUTE stmt;
+end$$
 
