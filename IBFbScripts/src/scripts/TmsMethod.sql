cfbc294b1a2ef1634488ad04d3bae39245237d8d
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-17 06:55:19 +0000

    GCP 4105 getListTmsMethod > As per Jeff, use cvterm to get list of methods
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3164 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/TmsMethod.sql
+++ IBFbScripts/src/scripts/TmsMethod.sql
@@ -6,23 +6,22 @@ IN v_tmethid int,
 IN v_tmname varchar(50),
 IN v_tmdesc varchar(255))
 begin
-	SET @sql := CONCAT("SELECT DISTINCT cvm.cvterm_id AS tmethid, ", 
-				"cvm.name AS tmname, ",
-				"cvm.definition AS tmdesc ",
-				"FROM cvterm_relationship cvr ",
-				"INNER JOIN cvterm cvm ON cvm.cvterm_id = cvr.object_id ",
-				"WHERE cvr.type_id = 1210 ");
+	SET @sql := CONCAT("SELECT cvterm_id AS tmethid, ",
+	   			"name AS tmname, ",
+       			"definition AS tmdesc ",
+  				"FROM cvterm "
+ 				"WHERE cv_id = 1020 ");
 
 	IF(v_tmethid IS NOT NULL) THEN
-		SET @sql = CONCAT(@sql," AND cvm.cvterm_id = ", v_tmethid);
+		SET @sql = CONCAT(@sql," AND cvterm_id = ", v_tmethid);
     END IF;
 
 	IF(v_tmname IS NOT NULL) THEN
-		SET @sql = CONCAT(@sql," AND cvm.name = '", v_tmname, "' ");
+		SET @sql = CONCAT(@sql," AND name = '", v_tmname, "' ");
     END IF;
 	
 	IF(v_tmdesc IS NOT NULL) THEN
-		SET @sql = CONCAT(@sql," AND cvm.definition = '", v_tmdesc, "' ");
+		SET @sql = CONCAT(@sql," AND definition = '", v_tmdesc, "' ");
     END IF;
 
 	SET @sql = CONCAT(@sql, " ORDER BY tmethid; ");
