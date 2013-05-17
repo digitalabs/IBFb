550109733b6ea9eea5f97ca903d7b2fd95b502fb
tiffany <tiffany@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-17 05:01:59 +0000

    GCP-4126
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3155 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Measuredin.sql
+++ IBFbScripts/src/scripts/Measuredin.sql
@@ -48,3 +48,25 @@ begin
 	EXECUTE stmt;
 end$$
 
+CREATE PROCEDURE `getMeasuredinByTraitidScaleidTmethid`(IN traitId int, IN scaleId int, IN tmethId int)
+BEGIN
+
+	SELECT
+	var.cvterm_id AS measuredinid
+	, traitrel.object_id AS traitid
+	, scalerel.object_id AS scaleid
+	, scalerel.object_id AS standardscale
+	, NULL AS report
+	, NULL AS formula
+	, methrel.object_id AS tmethid
+	FROM cvterm var
+	INNER JOIN cvterm_relationship traitrel ON traitrel.subject_id = var.cvterm_id AND traitrel.type_id = 1200
+	INNER JOIN cvterm_relationship methrel ON methrel.subject_id = var.cvterm_id AND methrel.type_id = 1210
+	INNER JOIN cvterm_relationship scalerel ON scalerel.subject_id = var.cvterm_id AND scalerel.type_id = 1220
+	WHERE var.cv_id = 1040
+	AND traitrel.object_id = traitId
+	AND methrel.object_id = tmethId
+	AND scalerel.object_id = scaleId
+	limit 1;
+
+END$$
