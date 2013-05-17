5a807d659052904f47baea2a31ce159c7513aca6
tiffany <tiffany@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-17 15:11:32 +0000

    GCP-4127 & GCP-4128
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3182 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Level.sql
+++ IBFbScripts/src/scripts/Level.sql
@@ -6,33 +6,33 @@ CREATE PROCEDURE `getLevelsByLabelId`(IN p_labelid int, IN isnumeric int, IN isc
 BEGIN
 IF iscentral = 1 THEN
 
-	SELECT
-	  labelid
-	  , factorId
+    SELECT
+      labelid
+      , factorid
       , levelno
       , lvalue
-	FROM 
+    FROM 
        v_level
     WHERE (isnumeric AND dtypeid IN (1110, 1117, 1118)
       OR NOT isnumeric AND dtypeid IN (1128, 1120))
       AND labelid = p_labelid
-  	ORDER BY levelno ASC
-	;
+    ORDER BY levelno ASC
+    ;
 
 ELSE
 
-	SELECT
-	  labelid
-	  , factorId
+    SELECT
+      labelid
+      , factorid
       , levelno
       , lvalue
-	FROM 
+    FROM 
        v_level
     WHERE (isnumeric AND dtypeid IN (1110, 1117, 1118)
       OR NOT isnumeric AND dtypeid IN (1128, 1120))
       AND labelid = p_labelid
-  	ORDER BY levelno DESC
-	;
+    ORDER BY levelno DESC
+    ;
 
 
 END IF;
