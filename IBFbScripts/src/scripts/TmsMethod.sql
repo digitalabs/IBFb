a31a6615e377ad53f02bfb88e27042a1b627d357
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-17 02:15:07 +0000

    GCP 4105 getListTmsMethod
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3149 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/TmsMethod.sql
+++ IBFbScripts/src/scripts/TmsMethod.sql
@@ -1,1 +1,32 @@
+delimiter $$
+drop procedure if exists `getListTmsMethod`$$
 
+CREATE PROCEDURE `getListTmsMethod` (
+IN v_tmethid int,
+IN v_tmname varchar(50),
+IN v_tmdesc varchar(255))
+begin
+	SET @sql := CONCAT("SELECT DISTINCT cvm.cvterm_id AS tmethid, ", 
+				"cvm.name AS tmname, ",
+				"cvm.definition AS tmdesc ",
+				"FROM cvterm_relationship cvr ",
+				"INNER JOIN cvterm cvm ON cvm.cvterm_id = cvr.object_id ",
+				"WHERE cvr.type_id = 1210 ");
+
+	IF(v_tmethid IS NOT NULL) THEN
+		SET @sql = CONCAT(@sql," AND cvm.cvterm_id = ", v_tmethid);
+    END IF;
+
+	IF(v_tmname IS NOT NULL) THEN
+		SET @sql = CONCAT(@sql," AND cvm.name = '", v_tmname, "' ");
+    END IF;
+	
+	IF(v_tmdesc IS NOT NULL) THEN
+		SET @sql = CONCAT(@sql," AND cvm.definition = '", v_tmdesc, "' ");
+    END IF;
+
+	SET @sql = CONCAT(@sql, " ORDER BY tmethid; ");
+	
+	PREPARE stmt FROM @sql;
+	EXECUTE stmt;
+end$$
