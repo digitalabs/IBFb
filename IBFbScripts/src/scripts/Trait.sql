aa17d07efca30e5234ae3530b6bdbe0aed02f893
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-17 07:07:35 +0000

    GCP 4270 getTraitList() > As per Jeff, use Cvterm.cv_id to get list of traits
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3165 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Trait.sql
+++ IBFbScripts/src/scripts/Trait.sql
@@ -12,10 +12,9 @@ begin
 			1 AS nstat, 
 			grp.name AS traitgroup
 	  FROM cvterm cvt
-	 INNER JOIN cvterm_relationship cvr ON cvr.object_id = cvt.cvterm_id
 	 INNER JOIN cvterm_relationship gcvr ON gcvr.subject_id = cvt.cvterm_id
 	 INNER JOIN cvterm grp ON grp.cvterm_id = gcvr.object_id
 	 WHERE gcvr.type_id = 1225 -- get "is a" relationship to get group name
-	   AND cvr.type_id = 1200 -- get "has property" relationships
+	   AND cvt.cv_id = 1010
 	 ORDER BY traitid;
 end$$
