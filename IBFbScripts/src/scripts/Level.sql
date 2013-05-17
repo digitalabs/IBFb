31eef095ecbaee8bcd9ae713c4e30bb00f72ef26
tiffany <tiffany@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-17 15:48:20 +0000

    GCP-4249 & GCP-4250
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3184 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Level.sql
+++ IBFbScripts/src/scripts/Level.sql
@@ -38,3 +38,49 @@ ELSE
 END IF;
 END$$
 
+
+DROP PROCEDURE IF EXISTS `searchLevels`$$
+
+CREATE PROCEDURE `searchLevels`(
+  IN labelid int
+  , IN levelno int
+  , IN factorid int
+  , IN lvalue1 double
+  , IN lvalue2 varchar(255)
+  , IN iscentral int)
+
+BEGIN
+
+  SET @sql := CONCAT("SELECT labelid, factorid, levelno, lvalue from v_level WHERE 1=1 ");
+
+  IF (labelid IS NOT NULL) THEN
+    SET @sql = CONCAT(@sql, " AND labelid = ", labelid);
+  END IF;
+
+  IF (levelno IS NOT NULL) THEN
+    SET @sql = CONCAT(@sql, " AND levelno = ", levelno);
+  END IF;
+
+  IF (factorid IS NOT NULL) THEN
+    SET @sql = CONCAT(@sql, " AND factorid = ", factorid);
+  END IF;
+  
+  IF (lvalue1 IS NOT NULL) THEN
+    SET @sql = CONCAT(@sql, " AND lvalue = ", lvalue1);
+  END IF;
+
+  IF (lvalue2 IS NOT NULL) THEN
+    SET @sql = CONCAT(@sql, " AND lvalue = ", lvalue2);
+  END IF;
+
+  IF iscentral = 1 THEN
+    SET @sql = CONCAT(@sql, " ORDER BY labelid, levelno ASC");
+  ELSE
+    SET @sql = CONCAT(@sql, " ORDER BY labelid, levelno DESC");
+  END IF;
+
+  PREPARE stmt FROM @sql;
+  EXECUTE stmt;
+
+
+END$$
