f28ecac5230b25c299ea5e7ad27025391f486c56
danielv <danielv@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-18 08:04:42 +0000

    (GCP-4251)
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3211 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Represtn.sql
+++ IBFbScripts/src/scripts/Represtn.sql
@@ -23,7 +23,7 @@ begin
 SET @myQuery = 'select distinct proj.project_id as represno, proj.name as represname, proj.project_id as effectid FROM
 project proj INNER JOIN project_relationship pr ON proj.project_id = pr.subject_project_id AND pr.type_id = 1150';
 
-if (studyId IS NOT NULL) THEN
+if (studyId IS NOT NULL AND studyId <> 0) THEN
   SET @myQuery = CONCAT(@myQuery, ' WHERE pr.object_project_id = ', studyId);
 END IF;
 
@@ -51,18 +51,28 @@ CREATE PROCEDURE `getReprestnForReprestn`(IN represno int, IN represname varchar
     SET @myQuery = 'select distinct proj.project_id as represno, proj.name as represname, proj.project_id as effectid FROM
 project proj INNER JOIN project_relationship pr ON proj.project_id = pr.subject_project_id AND pr.type_id = 1150';
 
-    if (represno IS NOT NULL) THEN
-      SET @myQuery = CONCAT(@myQuery, ' WHERE pr.object_project_id = ', studyId);
+    if (represno IS NOT NULL AND represno <> 0) THEN
+      SET @myQuery = CONCAT(@myQuery, ' WHERE proj.project_id = ', represno);
+      SET @varExists = true;
     END IF;
 
     IF (represname IS NOT NULL) THEN
-      IF (studyId IS NULL) THEN
-        SET @myQuery = CONCAT(@myQuery, ' WHERE ');
+      IF (@varExists IS NOT NULL and @varExists = true) THEN
+        SET @myQuery = CONCAT(@myQuery, ' AND');
       ELSE
-        SET @myQuery = CONCAT(@myQuery, ' AND ');
+        SET @myQuery = CONCAT(@myQuery, ' WHERE');
+        SET @varExists = true;
       END IF;
-
       SET @myQuery = CONCAT(@myQuery, ' proj.name =  ', quote(represname));
+    END IF;
+
+    IF (effectId IS NOT NULL and effectid <> 0) THEN
+      IF (@varExists IS NOT NULL and @varExists = true) THEN
+        SET @myQuery = CONCAT(@myQuery, ' AND');
+      ELSE
+        SET @myQuery = CONCAT(@myQuery, ' WHERE');
+      END IF;
+      SET @myQuery = CONCAT(@myQuery, ' proj.project_id = ', effectid);
     END IF;
 
     PREPARE stmt FROM @myQuery;
