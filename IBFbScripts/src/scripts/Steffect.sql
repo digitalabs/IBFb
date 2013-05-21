279f0568c0a60c4f617652a9309672ae293577c6
dennis <dennis@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-21 06:51:37 +0000

    added ; inside Steffect.sql -> getListSteffect, was getting an error in MySQL without it
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3284 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Steffect.sql
+++ IBFbScripts/src/scripts/Steffect.sql
@@ -29,10 +29,9 @@ drop procedure if exists `getListSteffect`$$
 
 CREATE PROCEDURE `getListSteffect`(IN studyid int)
 begin
-
 	select pr.subject_project_id as effectid, pr.object_project_id as studyid, "" as effectname
 	from project_relationship pr where pr.type_id = 1150
-	and pr.object_project_id = studyid
+	and pr.object_project_id = studyid;
 
 end$$
 
