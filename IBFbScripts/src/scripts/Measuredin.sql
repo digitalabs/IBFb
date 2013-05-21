ed4ba2b0ffc732c605cedc87f1e8480b78b76370
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-21 16:51:05 +0000

    GCP 4246 getMeasureinListByTraitid
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3305 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Measuredin.sql
+++ IBFbScripts/src/scripts/Measuredin.sql
@@ -7,41 +7,42 @@ IN v_traitid int,
 IN v_tmethid int,
 IN v_scaleid int)
 begin
-	SET @sql := CONCAT("SELECT cvt.cvterm_id AS measuredinid, ",
-				"crp.object_id AS traitid, ",
-				"crm.object_id AS tmethid, ",
-				"crs.object_id AS scaleid, ",
-				"crs.object_id AS standardscale ",
-				"FROM cvterm cvt ",
-				"INNER JOIN cvterm_relationship crp ON crp.subject_id = cvt.cvterm_id ",
-				"INNER JOIN cvterm_relationship crm ON crm.subject_id = cvt.cvterm_id ",
-				"INNER JOIN cvterm_relationship crs ON crs.subject_id = cvt.cvterm_id ",
-				"WHERE cvt.cv_id = 1040 ",
-				"AND crp.type_id = 1200 ",
-				"AND crm.type_id = 1210 ",
-				"AND crs.type_id = 1220 ");
+    SET @sql := CONCAT("SELECT cvt.cvterm_id AS measuredinid, ",
+                        "crp.object_id AS traitid, ",
+                        "crs.object_id AS scaleid, ",
+                        "crm.object_id AS tmethid ",
+                        "FROM cvterm cvt ",
+                        "INNER JOIN cvterm_relationship crp ON crp.subject_id = cvt.cvterm_id ",
+                        "INNER JOIN cvterm_relationship crm ON crm.subject_id = cvt.cvterm_id ",
+                        "INNER JOIN cvterm_relationship crs ON crs.subject_id = cvt.cvterm_id ",
+                        "WHERE cvt.cv_id = 1040 ",
+                        "AND crp.type_id = 1200 ",
+                        "AND crm.type_id = 1210 ",
+                        "AND crs.type_id = 1220 ");
 
-	IF(v_measuredinid IS NOT NULL) THEN
-		SET @sql = CONCAT(@sql," AND cvt.cvterm_id = ",v_measuredinid);
+    IF (v_measuredinid IS NOT NULL) THEN
+        SET @sql = CONCAT(@sql," AND cvt.cvterm_id = ",v_measuredinid);
     END IF;
 
-	IF(v_traitid IS NOT NULL) THEN
-		SET @sql = CONCAT(@sql," AND crp.object_id = ",v_traitid);
+    IF (v_traitid IS NOT NULL) THEN
+        SET @sql = CONCAT(@sql," AND crp.object_id = ",v_traitid);
     END IF;
 
-	IF(v_tmethid IS NOT NULL) THEN
-		SET @sql = CONCAT(@sql," AND crm.object_id = ",v_tmethid);
+    IF (v_tmethid IS NOT NULL) THEN
+        SET @sql = CONCAT(@sql," AND crm.object_id = ",v_tmethid);
     END IF;
 
-	IF(v_scaleid IS NOT NULL) THEN
-		SET @sql = CONCAT(@sql," AND crs.object_id = ",v_scaleid);
+    IF (v_scaleid IS NOT NULL) THEN
+        SET @sql = CONCAT(@sql," AND crs.object_id = ",v_scaleid);
     END IF;
 	
-	SET @sql = CONCAT(@sql, " ORDER BY measuredinid; ");
+    SET @sql = CONCAT(@sql, " ORDER BY measuredinid; ");
 	
-	PREPARE stmt FROM @sql;
-	EXECUTE stmt;
+    PREPARE stmt FROM @sql;
+    EXECUTE stmt;
 end$$
+
+drop procedure if exists `getMeasuredinByTraitidScaleidTmethid`$$
 
 CREATE PROCEDURE `getMeasuredinByTraitidScaleidTmethid`(IN traitId int, IN scaleId int, IN tmethId int)
 BEGIN
@@ -51,8 +52,8 @@ BEGIN
 	, traitrel.object_id AS traitid
 	, scalerel.object_id AS scaleid
 	, scalerel.object_id AS standardscale
-	, NULL AS report
-	, NULL AS formula
+        , NULL AS report
+        , NULL AS formula
 	, methrel.object_id AS tmethid
 	FROM cvterm var
 	INNER JOIN cvterm_relationship traitrel ON traitrel.subject_id = var.cvterm_id AND traitrel.type_id = 1200
@@ -63,5 +64,25 @@ BEGIN
 	AND methrel.object_id = tmethId
 	AND scalerel.object_id = scaleId
 	limit 1;
+
+END$$
+
+drop procedure if exists `getMeasuredinListByTraitid`$$
+
+CREATE PROCEDURE `getMeasuredinListByTraitid`(IN traitId int)
+BEGIN
+
+	SELECT
+	var.cvterm_id AS measuredinid
+	, traitrel.object_id AS traitid
+	, scalerel.object_id AS scaleid
+	, methrel.object_id AS tmethid
+	FROM cvterm var
+	INNER JOIN cvterm_relationship traitrel ON traitrel.subject_id = var.cvterm_id AND traitrel.type_id = 1200
+	INNER JOIN cvterm_relationship methrel ON methrel.subject_id = var.cvterm_id AND methrel.type_id = 1210
+	INNER JOIN cvterm_relationship scalerel ON scalerel.subject_id = var.cvterm_id AND scalerel.type_id = 1220
+	WHERE var.cv_id = 1040
+	AND traitrel.object_id = traitId
+        ORDER BY measuredinid;
 
 END$$
