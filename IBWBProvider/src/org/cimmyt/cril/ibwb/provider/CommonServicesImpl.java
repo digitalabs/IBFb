114f7f1fde477c74a7cf20cb8c16160c04498517
joyce <joyce@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-21 00:34:53 +0000

    GCP-4125 Refactor getDmsattrByDmsatrecAndDmsatype(Dmsattr dmsattr)
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3280 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBWBProvider/src/org/cimmyt/cril/ibwb/provider/CommonServicesImpl.java
+++ IBWBProvider/src/org/cimmyt/cril/ibwb/provider/CommonServicesImpl.java
@@ -608,15 +608,7 @@ public class CommonServicesImpl implements CommonServices {
     }
 
     public Dmsattr getDmsattrByDmsatrecAndDmsatype(Dmsattr dmsattr) {
-        //return dmsattrDAO.getDmsattrByDmsatrecAndDmsatype(dmsattr);
-        if(isLocal()) {
-            dmsattr.setIslocal(1);
-    	} else {
-            dmsattr.setIslocal(0);
-    	}
-
-    	return this.utilityDAO.callStoredProcedureForObject(dmsattr, "getDmsattrByDmsatrecAndDmsatype", new String[]{"dmsatrec","dmsatype", "islocal"}, 
-    			new String[] {"dmsatid", "dmsatyp", "dmsatab", "dmsatrec", "dmsatval"});
+        return dmsattrDAO.getDmsattrByDmsatrecAndDmsatype(dmsattr);
     }
 
     @Override
