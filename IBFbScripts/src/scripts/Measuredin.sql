f4142decc1eea279dbd30b8c77473c6b82b434b0
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-23 10:25:36 +0000

    reverted changes related to GCP 4347
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3357 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Measuredin.sql
+++ IBFbScripts/src/scripts/Measuredin.sql
@@ -5,9 +5,7 @@ CREATE PROCEDURE `getListMeasuredIn` (
 IN v_measuredinid int,
 IN v_traitid int,
 IN v_tmethid int,
-IN v_scaleid int,
-IN v_central_db_name varchar(20),
-IN v_is_local int)
+IN v_scaleid int)
 begin
 
     -- DOES NOT CHECK v_is_local, no info needed from central db ontology
