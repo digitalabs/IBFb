7c5223ec6ecd7174852b3e509eee46132d976fd5
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-25 10:05:13 +0000

    updated constants to include scale
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3485 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBWBProvider/src/org/cimmyt/cril/ibwb/provider/utils/ChadoSchemaUtil.java
+++ IBWBProvider/src/org/cimmyt/cril/ibwb/provider/utils/ChadoSchemaUtil.java
@@ -6,20 +6,44 @@ public class ChadoSchemaUtil {
     public static final String STUDY = "STUDY";
     public static final String STUDY_NAME = "STUDY NAME";
     
-    public static final String TRIAL_INSTANCE = "TRIAL INSTANCE"; 
+    public static final String TRIAL_INSTANCE = "TRIALINSTANCE"; 
+    public static final String TRIAL_INSTANCE_NUMBER = "TRIALINSTANCENUMBER"; 
+    
     public static final String NURSERY = "NURSERY";
+    public static final String NURSERY_NUMBER = "NURSERYNUMBER";
     
-    public static final String GERMPLASM_ENTRY = "GERMPLASM ENTRY";
+    public static final String GERMPLASM_ENTRY = "GERMPLASMENTRY";
+    public static final String GERMPLASM_ENTRY_NUMBER = "GERMPLASMENTRYNUMBER";
+    public static final String GERMPLASM_ENTRY_NESTED_NUMBER = "GERMPLASMENTRYNESTEDNUMBER";
+    public static final String GERMPLASM_ENTRY_CODE = "GERMPLASMENTRYCODE";
+    
     public static final String GERMPLASM_ID = "GERMPLASM ID";
+    public static final String GERMPLASM_ID_DBCV = "GERMPLASMIDDBCV";
+    public static final String GERMPLASM_ID_DBID = "GERMPLASMIDDBID";
     
-    public static final String FIELD_PLOT = "FIELD PLOT";
-    public static final String REPLICATION_FACTOR = "REPLICATION FACTOR";
-    public static final String BLOCKING_FACTOR = "BLOCKING FACTOR";
+    public static final String CROSS_HISTORY = "CROSSHISTORY";
+    public static final String CROSS_HISTORY_NAME = "CROSSHISTORYNAME";
     
-    public static final String ROW_IN_LAYOUT = "ROW IN LAYOUT";
-    public static final String COLUMN_IN_LAYOUT = "COLUMN IN LAYOUT";
+    public static final String SEED_SOURCE = "SEEDSOURCE";
+    public static final String SEED_SOURCE_NAME = "SEEDSOURCENAME";
     
-    public static final String[] TRIAL_ENVT_PROPS = {TRIAL_INSTANCE, NURSERY};
+    public static final String FIELD_PLOT = "FIELDPLOT";
+    public static final String FIELD_PLOT_NUMBER = "FIELDPLOTNUMBER";
+    public static final String FIELD_PLOT_NESTED_NUMBER = "FIELDPLOTNEUPSTEDNUMBER";
+        
+    public static final String REPLICATION_FACTOR = "REPLICATIONFACTOR";
+    public static final String REPLICATION_FACTOR_NUMBER = "REPLICATIONFACTORNUMBER";
+    
+    public static final String BLOCKING_FACTOR = "BLOCKINGFACTOR";
+    public static final String BLOCKING_FACTOR_NUMBER = "BLOCKINGFACTORNUMBER";
+    
+    public static final String ROW_IN_LAYOUT = "ROWINLAYOUT";
+    public static final String ROW_IN_LAYOUT_NUMBER = "ROWINLAYOUTNUMBER";
+    
+    public static final String COLUMN_IN_LAYOUT = "COLUMNINLAYOUT";
+    public static final String COLUMN_IN_LAYOUT_NUMBER = "COLUMNINLAYOUTNUMBER";
+    
+    public static final String[] TRIAL_ENVT_PROPS = {TRIAL_INSTANCE, NURSERY_NUMBER};
     public static final String[] TRIAL_DESIGN_PROPS = {FIELD_PLOT, REPLICATION_FACTOR, BLOCKING_FACTOR};
     public static final String[] GERMPLASM_PROPS = {GERMPLASM_ENTRY, GERMPLASM_ID};
     
@@ -30,8 +54,11 @@ public class ChadoSchemaUtil {
     public static final int GERMPLASM_VAR_TYPE = 1040;
     public static final int OBSERVATION_VARIATE_TYPE = 1043;
     
+     public static final int DEFAULT_TRAIT_GROUP = 1050;
+    
     
     public static int getStoredInVariableType(String property){
+        
         int varType = TRIAL_DESIGN_VAR_TYPE;
         String compareStr = property.trim().toUpperCase();
         
