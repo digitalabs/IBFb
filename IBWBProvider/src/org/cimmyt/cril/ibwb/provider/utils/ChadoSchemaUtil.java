01e5034cad0c7337ecaf589492fd336866e7af10
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-29 08:38:38 +0000

    GCP 4418 refactored trait+scale+method constants to fit new schema
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3569 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBWBProvider/src/org/cimmyt/cril/ibwb/provider/utils/ChadoSchemaUtil.java
+++ IBWBProvider/src/org/cimmyt/cril/ibwb/provider/utils/ChadoSchemaUtil.java
@@ -1,52 +1,71 @@
 package org.cimmyt.cril.ibwb.provider.utils;
 
+import ibfb.domain.core.Workbook;
 import org.apache.commons.lang.ArrayUtils;
 
 public class ChadoSchemaUtil {
-    public static final String STUDY = "STUDY";
-    public static final String STUDY_NAME = "STUDY NAME";
+//    public static final String STUDY = "STUDY";
+//    public static final String STUDY_NAME = "STUDY NAME";
+//    
+//    public static final String TRIAL_INSTANCE = "TRIALINSTANCE"; 
+//    public static final String TRIAL_INSTANCE_NUMBER = "TRIALINSTANCENUMBER"; 
+//    
+//    public static final String NURSERY = "NURSERY";
+//    public static final String NURSERY_NUMBER = "NURSERYNUMBER";
+//    
+//    public static final String GERMPLASM_ENTRY = "GERMPLASMENTRY";
+//    public static final String GERMPLASM_ENTRY_NUMBER = "GERMPLASMENTRYNUMBER";
+//    public static final String GERMPLASM_ENTRY_NESTED_NUMBER = "GERMPLASMENTRYNESTEDNUMBER";
+//    public static final String GERMPLASM_ENTRY_CODE = "GERMPLASMENTRYCODE";
+//    
+//    public static final String GERMPLASM_ID = "GERMPLASM ID";
+//    public static final String GERMPLASM_ID_DBCV = "GERMPLASMIDDBCV";
+//    public static final String GERMPLASM_ID_DBID = "GERMPLASMIDDBID";
+//    
+//    public static final String CROSS_HISTORY = "CROSSHISTORY";
+//    public static final String CROSS_HISTORY_NAME = "CROSSHISTORYNAME";
+//    
+//    public static final String SEED_SOURCE = "SEEDSOURCE";
+//    public static final String SEED_SOURCE_NAME = "SEEDSOURCENAME";
+//    
+//    public static final String FIELD_PLOT = "FIELDPLOT";
+//    public static final String FIELD_PLOT_NUMBER = "FIELDPLOTNUMBER";
+//    public static final String FIELD_PLOT_NESTED_NUMBER = "FIELDPLOTNEUPSTEDNUMBER";
+//        
+//    public static final String REPLICATION_FACTOR = "REPLICATIONFACTOR";
+//    public static final String REPLICATION_FACTOR_NUMBER = "REPLICATIONFACTORNUMBER";
+//    
+//    public static final String BLOCKING_FACTOR = "BLOCKINGFACTOR";
+//    public static final String BLOCKING_FACTOR_NUMBER = "BLOCKINGFACTORNUMBER";
+//    
+//    public static final String ROW_IN_LAYOUT = "ROWINLAYOUT";
+//    public static final String ROW_IN_LAYOUT_NUMBER = "ROWINLAYOUTNUMBER";
+//    
+//    public static final String COLUMN_IN_LAYOUT = "COLUMNINLAYOUT";
+//    public static final String COLUMN_IN_LAYOUT_NUMBER = "COLUMNINLAYOUTNUMBER";
+//    
+    public static final String[] TRIAL_ENVT_PROPS = {
+        Workbook.TRIAL_INSTANCE, Workbook.NURSERY
+    };
+    public static final String[] TRIAL_ENVT_PROPS_SCALES = {
+        Workbook.TRIAL_INSTANCE_NUMBER, Workbook.NURSERY_NUMBER
+    };
     
-    public static final String TRIAL_INSTANCE = "TRIALINSTANCE"; 
-    public static final String TRIAL_INSTANCE_NUMBER = "TRIALINSTANCENUMBER"; 
+    public static final String[] TRIAL_DESIGN_PROPS = {
+        Workbook.FIELD_PLOT, Workbook.REPLICATION_FACTOR, Workbook.BLOCKING_FACTOR
+    };
+    public static final String[] TRIAL_DESIGN_PROPS_SCALES = {
+        Workbook.FIELD_PLOT_NUMBER, Workbook.FIELD_PLOT_NESTED_NUMBER, 
+        Workbook.REPLICATION_FACTOR_NUMBER, Workbook.BLOCKING_FACTOR_NUMBER
+    };
     
-    public static final String NURSERY = "NURSERY";
-    public static final String NURSERY_NUMBER = "NURSERYNUMBER";
-    
-    public static final String GERMPLASM_ENTRY = "GERMPLASMENTRY";
-    public static final String GERMPLASM_ENTRY_NUMBER = "GERMPLASMENTRYNUMBER";
-    public static final String GERMPLASM_ENTRY_NESTED_NUMBER = "GERMPLASMENTRYNESTEDNUMBER";
-    public static final String GERMPLASM_ENTRY_CODE = "GERMPLASMENTRYCODE";
-    
-    public static final String GERMPLASM_ID = "GERMPLASM ID";
-    public static final String GERMPLASM_ID_DBCV = "GERMPLASMIDDBCV";
-    public static final String GERMPLASM_ID_DBID = "GERMPLASMIDDBID";
-    
-    public static final String CROSS_HISTORY = "CROSSHISTORY";
-    public static final String CROSS_HISTORY_NAME = "CROSSHISTORYNAME";
-    
-    public static final String SEED_SOURCE = "SEEDSOURCE";
-    public static final String SEED_SOURCE_NAME = "SEEDSOURCENAME";
-    
-    public static final String FIELD_PLOT = "FIELDPLOT";
-    public static final String FIELD_PLOT_NUMBER = "FIELDPLOTNUMBER";
-    public static final String FIELD_PLOT_NESTED_NUMBER = "FIELDPLOTNEUPSTEDNUMBER";
-        
-    public static final String REPLICATION_FACTOR = "REPLICATIONFACTOR";
-    public static final String REPLICATION_FACTOR_NUMBER = "REPLICATIONFACTORNUMBER";
-    
-    public static final String BLOCKING_FACTOR = "BLOCKINGFACTOR";
-    public static final String BLOCKING_FACTOR_NUMBER = "BLOCKINGFACTORNUMBER";
-    
-    public static final String ROW_IN_LAYOUT = "ROWINLAYOUT";
-    public static final String ROW_IN_LAYOUT_NUMBER = "ROWINLAYOUTNUMBER";
-    
-    public static final String COLUMN_IN_LAYOUT = "COLUMNINLAYOUT";
-    public static final String COLUMN_IN_LAYOUT_NUMBER = "COLUMNINLAYOUTNUMBER";
-    
-    public static final String[] TRIAL_ENVT_PROPS = {TRIAL_INSTANCE, NURSERY_NUMBER};
-    public static final String[] TRIAL_DESIGN_PROPS = {FIELD_PLOT, REPLICATION_FACTOR, BLOCKING_FACTOR};
-    public static final String[] GERMPLASM_PROPS = {GERMPLASM_ENTRY, GERMPLASM_ID};
-    
+    public static final String[] GERMPLASM_PROPS = {
+        Workbook.GERMPLASM_ENTRY, Workbook.GERMPLASM_ID
+    };
+    public static final String[] GERMPLASM_PROPS_SCALES = {
+        Workbook.GERMPLASM_ENTRY_CODE, Workbook.GERMPLASM_ENTRY_NUMBER, Workbook.GERMPLASM_ENTRY_NESTED_NUMBER,
+        Workbook.GERMPLASM_ID_DBCV, Workbook.GERMPLASM_ID_DBID
+    };
     public static final int STUDY_VAR_TYPE = 1010;
     public static final int DATASET_VAR_TYPE = 1015;
     public static final int TRIAL_ENVT_VAR_TYPE = 1020;
@@ -54,13 +73,42 @@ public class ChadoSchemaUtil {
     public static final int GERMPLASM_VAR_TYPE = 1040;
     public static final int OBSERVATION_VARIATE_TYPE = 1043;
     
-     public static final int DEFAULT_TRAIT_GROUP = 1050;
+    public static final int DEFAULT_TRAIT_GROUP = 1050;
     
+//    private static final int INIT_VAR_TYPE = -1;
+    /**
+     * Get stored in variable type from combination of property and scale.
+     * If no match from property-scale combination, perform matching by
+     * property name.
+     * 
+     * @param property
+     * @param scale
+     * @return 
+     */ 
+    public static int getStoredInVariableType(String property, String scale){
+        int varType = -1;
+        String compareStr = Workbook.getStringWithOutBlanks(property + scale);
+        
+        if (ArrayUtils.contains(TRIAL_ENVT_PROPS_SCALES, compareStr)){
+            varType = TRIAL_ENVT_VAR_TYPE;
+            
+        } else if (ArrayUtils.contains(TRIAL_DESIGN_PROPS_SCALES, compareStr)){
+            varType = TRIAL_DESIGN_VAR_TYPE;
+            
+        } else if (ArrayUtils.contains(GERMPLASM_PROPS_SCALES, compareStr)){
+            varType = GERMPLASM_VAR_TYPE;
+        }
+        
+        if (varType == -1){
+            varType = getStoredInVariableType(property);
+        }
+               
+        return varType;
+    }
     
     public static int getStoredInVariableType(String property){
-        
         int varType = TRIAL_DESIGN_VAR_TYPE;
-        String compareStr = property.trim().toUpperCase();
+        String compareStr = Workbook.getStringWithOutBlanks(property);
         
         if (ArrayUtils.contains(TRIAL_ENVT_PROPS, compareStr)){
             varType = TRIAL_ENVT_VAR_TYPE;
