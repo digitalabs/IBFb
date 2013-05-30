0dc5483d2b601a4fe78bfc1d6e4b6ae8c228a90d
daniel <daniel@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-05-30 08:02:26 +0000

    Merge for 5/30/2013
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3603 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBWBCommonGui/src/org/cimmyt/cril/ibwb/commongui/AppConstants.java
+++ IBWBCommonGui/src/org/cimmyt/cril/ibwb/commongui/AppConstants.java
@@ -6,4 +6,26 @@ package org.cimmyt.cril.ibwb.commongui;
  */
 public class AppConstants {
     public static boolean SHOW_IMPORT_GERMPLASM = false;
+    private static final String BREEDING_MANAGER = "BREEDING_MANAGER";
+    private static final String FIELDBOOK_TOOLS  = "FIELDBOOK_TOOLS";
+    private static final String BOTH_APPS = "BOTH_APPS";
+    
+    public static String CURRENT_APP = BOTH_APPS;
+    
+    public static boolean onlyBreeding() {
+        return CURRENT_APP.equals(BREEDING_MANAGER);
+    }
+
+    public static boolean onlyFieldbook() {
+        return CURRENT_APP.equals(FIELDBOOK_TOOLS);
+    }
+    
+    public static boolean bothApps() {
+        return CURRENT_APP.equals(BOTH_APPS);
+    }
+
+    public static void setCurrentApp(String appName) {
+        CURRENT_APP = appName;
+    }
+    
 }
