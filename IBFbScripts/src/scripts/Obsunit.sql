177184810491cc529de3ce7b5fae934b2afd97ff
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-06-01 03:38:47 +0000

    fixed return fields in getObsunitListByEffectid
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3677 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Obsunit.sql
+++ IBFbScripts/src/scripts/Obsunit.sql
@@ -24,7 +24,7 @@ begin
 IF iscentral = 1 then
 
 	select 
-project_id as effect_id, 
+project_id as effectid, 
 nd_experiment_id as ounitid 
 from nd_experiment_project nep 
 inner join project_relationship pr on pr.subject_project_id = nep.project_id and pr.type_id = 1150 
@@ -34,7 +34,7 @@ order by ounitid asc;
 else
 
 	select 
-project_id as effect_id, 
+project_id as effectid, 
 nd_experiment_id as ounitid 
 from nd_experiment_project nep 
 inner join project_relationship pr on pr.subject_project_id = nep.project_id and pr.type_id = 1150 
