d70c614f79681be9140073429ead445a8840ad27
darla <darla@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-06-08 06:50:25 +0000

    GCP 4517 added optional handlng for REPLICATION vs REPLICATION FACTOR and BLOCK vs BLOCKING FACTOR as headers
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3888 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbDomain/src/ibfb/domain/core/Workbook.java
+++ IBFbDomain/src/ibfb/domain/core/Workbook.java
@@ -74,10 +74,15 @@ public class Workbook {
     public static final String FIELD_PLOT_NESTED_NUMBER_ENUMERATED_N = FIELD_PLOT_NESTED_NUMBER + ENUMERATED_N;
 
     public static final String REPLICATION = "REPLICATION";
+    public static final String REPLICATION_NUMBER = "REPLICATIONNUMBER";
+   
     public static final String REPLICATION_FACTOR = "REPLICATIONFACTOR";
     public static final String REPLICATION_FACTOR_NUMBER = "REPLICATIONFACTORNUMBER";
     
     public static final String BLOCK = "BLOCK";
+    public static final String BLOCK_NUMBER = "BLOCKNUMBER";
+    public static final String BLOCK_NESTED_NUMBER = "BLOCKNESTEDNUMBER";
+    
     public static final String BLOCKING_FACTOR = "BLOCKINGFACTOR";
     public static final String BLOCKING_FACTOR_NUMBER = "BLOCKINGFACTORNUMBER";
     public static final String BLOCKING_FACTOR_NESTED_NUMBER = "BLOCKINGFACTORNESTEDNUMBER";
