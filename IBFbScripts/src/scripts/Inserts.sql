062adb58b6467f877b4955deb5aa1b900d13e1b3
glenn <glenn@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-06-10 06:51:47 +0000

    changed INSERT INTO queries to REPLACE INTO
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3913 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Inserts.sql
+++ IBFbScripts/src/scripts/Inserts.sql
@@ -1,151 +1,41 @@
-insert into cv(cv_id,name,definition)
-select * from (select 1000 as cv_id,'IBDB TERMS' as name,
-'CV of terms used to annotate relationships and identify objects in the ibdb database' as definition) as tmp
-where not exists (select cv_id from cv where cv_id = 1000) limit 1;
+REPLACE INTO cv(cv_id,name,definition) VALUES
+ (1000, 'IBDB TERMS', 'CV of terms used to annotate relationships and identify objects in the ibdb database')
+,(2005,'8006','Study status - Assigned (code)')
+,(2010,'8070','Study type - assigned (type)');
 
-insert into cv(cv_id,name,definition)
-select * from (select 2005 as cv_id,'8006' as name,
-'Study status - Assigned (code)' as definition) as tmp
-where not exists (select cv_id from cv where cv_id = 2005) limit 1;
-
-insert into cv(cv_id,name,definition)
-select * from (select 2010 as cv_id,'8070' as name,
-'Study type - assigned (type)' as definition) as tmp
-where not exists (select cv_id from cv where cv_id = 2010) limit 1;		
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 12960 as cvterm_id,2005 as cv_id,'1' as name,
-'Active study visable to all users with access' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 12960) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 12970 as cvterm_id,2005 as cv_id,'2' as name,
-'Active study visable to owner only' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 12970) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 12980 as cvterm_id,2005 as cv_id,'3' as name,
-'Locked study visable to all users with access' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 12980) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 12990 as cvterm_id,2005 as cv_id,'9' as name,
-'Deleted study' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 12990) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10000 as cvterm_id,2010 as cv_id,'N' as name,
-'Nursery' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10000) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10001 as cvterm_id,2010 as cv_id,'HB' as name,
-'Hybridization nursery' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10001) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10002 as cvterm_id,2010 as cv_id,'PN' as name,
-'Pedigree nursery' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10002) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10003 as cvterm_id,2010 as cv_id,'CN' as name,
-'Characterization nursery' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10003) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10005 as cvterm_id,2010 as cv_id,'OYT' as name,
-'Observational yield trial' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10005) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10007 as cvterm_id,2010 as cv_id,'BON' as name,
-'BULU observational nursery' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10007) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10010 as cvterm_id,2010 as cv_id,'T' as name,
-'Trial' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10010) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10015 as cvterm_id,2010 as cv_id,'RYT' as name,
-'Replicated yield trial' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10015) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10017 as cvterm_id,2010 as cv_id,'OFT' as name,
-'On farm trial' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10017) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10020 as cvterm_id,2010 as cv_id,'S' as name,
-'Survey' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10020) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 10030 as cvterm_id,2010 as cv_id,'E' as name,
-'Experiment' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 10030) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 1110 as cvterm_id,1000 as cv_id,'Numeric variable' as name,
-'Variable with numeric values either continuous or integer' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 1110) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 1113 as cvterm_id,1000 as cv_id,'Minimum value' as name,
-'Minimum value allowed for a numeric variable' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 1113) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 1115 as cvterm_id,1000 as cv_id,'Maximum value' as name,
-'Maximum value allowed for a numeric variable' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 1115) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 1117 as cvterm_id,1000 as cv_id,'Date variable' as name,
-'Date - numeric value in format yyyymmdd with least significant parts set to zero acording to precision' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 1117) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 1118 as cvterm_id,1000 as cv_id,'Numeric DBID  variable' as name,
-'Integer database ID (may be negative)' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 1118) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 1120 as cvterm_id,1000 as cv_id,'Character  variable' as name,
-'Variable with character values' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 1120) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 1125 as cvterm_id,1000 as cv_id,'Timestamp  variable' as name,
-'Character variable in format yyyy-mm-dd:hh:mm:ss:nnn with least significant parts omitted acording to precision' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 1125) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 1128 as cvterm_id,1000 as cv_id,'Character DBID  variable' as name,
-'Character database ID' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 1128) limit 1;
-
-insert into cvterm(cvterm_id,cv_id,name,definition,is_obsolete,is_relationshiptype)
-select * from (select 1130 as cvterm_id,1000 as cv_id,'Categorical  variable' as name,
-'Variable with discrete class values (numeric or character all treated as character)' as definition,0 as is_obsolete,0 as is_relationshiptype) as tmp
-where not exists (select cvterm_id from cvterm where cvterm_id = 1130) limit 1;
-
+REPLACE INTO cvterm(cvterm_id,cv_id,name,definition,dbxref_id,is_obsolete,is_relationshiptype) VALUES
+ (1110,1000,'Numeric variable','Variable with numeric values either continuous or integer',null,0,0)
+,(1113,1000,'Minimum value','Minimum value allowed for a numeric variable',null,0,0)
+,(1115,1000,'Maximum value','Maximum value allowed for a numeric variable',null,0,0)
+,(1117,1000,'Date variable','Date - numeric value in format yyyymmdd with least significant parts set to zero acording to precision',null,0,0)
+,(1118,1000,'Numeric DBID  variable','Integer database ID (may be negative)',null,0,0)
+,(1120,1000,'Character  variable','Variable with character values',null,0,0)
+,(1125,1000,'Timestamp  variable','Character variable in format yyyy-mm-dd:hh:mm:ss:nnn with least significant parts omitted acording to precision',null,0,0)
+,(1128,1000,'Character DBID  variable','Character database ID',null,0,0)
+,(1130,1000,'Categorical  variable','Variable with discrete class values (numeric or character all treated as character)',null,0,0)
+,(10000,2010,'N','Nursery',null,0,0)
+,(10001,2010,'HB','Hybridization nursery',null,0,0)
+,(10002,2010,'PN','Pedigree nursery',null,0,0)
+,(10003,2010,'CN','Characterization nursery',null,0,0)
+,(10005,2010,'OYT','Observational yield trial',null,0,0)
+,(10007,2010,'BON','BULU observational nursery',null,0,0)
+,(10010,2010,'T','Trial',null,0,0)
+,(10015,2010,'RYT','Replicated yield trial',null,0,0)
+,(10017,2010,'OFT','On farm trial',null,0,0)
+,(10020,2010,'S','Survey',null,0,0)
+,(10030,2010,'E','Experiment',null,0,0)
+,(12960,2005,'1','Active study visable to all users with access',null,0,0)
+,(12970,2005,'2','Active study visable to owner only',null,0,0)
+,(12980,2005,'3','Locked study visable to all users with access',null,0,0)
+,(12990,2005,'9','Deleted study',null,0,0)
+;
 
 REPLACE INTO nd_geolocation (nd_geolocation_id, description) VALUES
 (1, '0');
 
-INSERT INTO project(project_id,name,description)
-SELECT * FROM (SELECT -1 as project_id,'FB Studies' as name,'FB Studies' as description) as tmp
-WHERE NOT EXISTS (SELECT project_id from project where project_id = -1) limit 1;
-	
-INSERT INTO project_relationship(project_relationship_id,type_id,object_project_id,subject_project_id)
-SELECT * FROM (SELECT -1 as project_id,1140 as type_id,1 as object_project_id,-1 as subject_project_id) as tmp
-WHERE NOT EXISTS (SELECT project_relationship_id from project_relationship where subject_project_id = -1) limit 1;
+REPLACE INTO project(project_id,name,description) VALUES
+(-1,'FB Studies','FB Studies');
 
-
-
-
+REPLACE INTO project_relationship(project_relationship_id,type_id,object_project_id,subject_project_id) VALUES
+(-1, 1140, 1, -1);
 
