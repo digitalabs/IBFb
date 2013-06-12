7d2454f8bc7201c62eb89beb2fcd243fbd25bd0d
dennis <dennis@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-06-12 01:54:22 +0000

    added constraint checker on Steffect.sql
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@3947 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Steffect.sql
+++ IBFbScripts/src/scripts/Steffect.sql
@@ -50,35 +50,68 @@ START TRANSACTION;
 
 CALL getNextMinReturn('project',v_effectid);
 
-INSERT INTO project(project_id,name,description)
-VALUES(v_effectid,v_effectname,v_effectname);
+
+-- PROJECT unique constraint | NAME
+IF NOT EXISTS (SELECT 1 FROM project WHERE name=v_effectname) THEN
+  INSERT INTO project(project_id,name,description)
+  VALUES(v_effectid,v_effectname,v_effectname);
+END IF;
 
 CALL getNextMinReturn('project_relationship',v_project_relationship_id);
 
-INSERT INTO project_relationship(project_relationship_id,type_id,object_project_id,subject_project_id)
-VALUE(v_project_relationship_id,1150,v_studyid,v_effectid);
+-- PROJECT_RELATIONSHIP unique constraint | SUBJECT_PROJECT_ID, OBJECT_PROJECT_ID, TYPE_ID
+IF NOT EXISTS (SELECT 1 FROM project_relationship WHERE subject_project_id=v_effectid AND object_project_id=v_studyid AND type_id=1150) THEN
+  INSERT INTO project_relationship(project_relationship_id,type_id,object_project_id,subject_project_id)
+  VALUE(v_project_relationship_id,1150,v_studyid,v_effectid);
+END IF;
 
 CALL getNextMinReturn('projectprop',v_projectprop_id);
 
-INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
-VALUES(v_projectprop_id,v_effectid,1016,'DATASET',1);
-SET v_projectprop_id := v_projectprop_id - 1;
-INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
-VALUES(v_projectprop_id,v_effectid,1060,'DATASET NAME',1);
-SET v_projectprop_id := v_projectprop_id - 1;
-INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
-VALUES(v_projectprop_id,v_effectid,1070,'8150',1);
+-- PROJECTPROP unique constraint | PROJECT_ID, TYPE_ID, RANK
+IF NOT EXISTS (SELECT 1 FROM projectprop WHERE project_id=v_effectid AND type_id=1016 AND rank=1) THEN
+  INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
+  VALUES(v_projectprop_id,v_effectid,1016,'DATASET',1);
+END IF;
 
 SET v_projectprop_id := v_projectprop_id - 1;
-INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
-VALUES(v_projectprop_id,v_effectid,1016,'DATASET TITLE',2);
-SET v_projectprop_id := v_projectprop_id - 1;
-INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
-VALUES(v_projectprop_id,v_effectid,1060,'DATASET TITLE',2);
-SET v_projectprop_id := v_projectprop_id - 1;
-INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
-VALUES(v_projectprop_id,v_effectid,1070,'8155',2);
 
+-- PROJECTPROP unique constraint | PROJECT_ID, TYPE_ID, RANK
+IF NOT EXISTS (SELECT 1 FROM projectprop WHERE project_id=v_effectid AND type_id=1060 AND rank=1) THEN
+  INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
+  VALUES(v_projectprop_id,v_effectid,1060,'DATASET NAME',1);
+END IF;
+
+SET v_projectprop_id := v_projectprop_id - 1;
+
+-- PROJECTPROP unique constraint | PROJECT_ID, TYPE_ID, RANK
+IF NOT EXISTS (SELECT 1 FROM projectprop WHERE project_id=v_effectid AND type_id=1070 AND rank=1) THEN
+  INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
+  VALUES(v_projectprop_id,v_effectid,1070,'8150',1);
+END IF;
+
+SET v_projectprop_id := v_projectprop_id - 1;
+
+-- PROJECTPROP unique constraint | PROJECT_ID, TYPE_ID, RANK
+IF NOT EXISTS (SELECT 1 FROM projectprop WHERE project_id=v_effectid AND type_id=1016 AND rank=2) THEN
+  INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
+  VALUES(v_projectprop_id,v_effectid,1016,'DATASET TITLE',2);
+END IF;
+
+SET v_projectprop_id := v_projectprop_id - 1;
+
+-- PROJECTPROP unique constraint | PROJECT_ID, TYPE_ID, RANK
+IF NOT EXISTS (SELECT 1 FROM projectprop WHERE project_id=v_effectid AND type_id=1060 AND rank=2) THEN
+  INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
+  VALUES(v_projectprop_id,v_effectid,1060,'DATASET TITLE',2);
+END IF;
+
+SET v_projectprop_id := v_projectprop_id - 1;
+
+-- PROJECTPROP unique constraint | PROJECT_ID, TYPE_ID, RANK
+IF NOT EXISTS (SELECT 1 FROM projectprop WHERE project_id=v_effectid AND type_id=1070 AND rank=2) THEN
+  INSERT INTO projectprop(projectprop_id,project_id,type_id,value,rank)
+  VALUES(v_projectprop_id,v_effectid,1070,'8155',2);
+END IF;
 
 COMMIT;
 
