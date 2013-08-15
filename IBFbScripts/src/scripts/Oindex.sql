8aa7e8b63388081f82ae2da89428d4a4b57d7e50
tiffany <tiffany@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-08-15 10:29:39 +0000

    opti
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@4244 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Oindex.sql
+++ IBFbScripts/src/scripts/Oindex.sql
@@ -4,14 +4,15 @@ drop procedure if exists addOindex$$
 
 CREATE PROCEDURE addOindex(
 IN v_nd_experiment_id int,
-IN v_project_id int)
+IN v_project_id int,
+IN v_nd_experiment_project_id int)
 begin
 
-DECLARE v_nd_experiment_project_id int;
+-- DECLARE v_nd_experiment_project_id int;
 
 -- START TRANSACTION;
 
-CALL getNextMinReturn('nd_experiment_project',v_nd_experiment_project_id);
+-- CALL getNextMinReturn('nd_experiment_project',v_nd_experiment_project_id);
 
 INSERT INTO nd_experiment_project(nd_experiment_project_id,project_id,nd_experiment_id)
 VALUES(v_nd_experiment_project_id,v_project_id,v_nd_experiment_id);
