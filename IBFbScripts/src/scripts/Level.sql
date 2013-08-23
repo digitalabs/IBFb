e6ea1ccdae2a974248e2dd08927d0d0987a3d69c
tiffany <tiffany@4ebb2e00-0138-4608-bc6e-edf3948d4863>
2013-08-23 02:08:12 +0000

    opti
    git-svn-id: https://efficio.atlassian.net/svn/GCP/trunk/IBFb@4317 4ebb2e00-0138-4608-bc6e-edf3948d4863

--- IBFbScripts/src/scripts/Level.sql
+++ IBFbScripts/src/scripts/Level.sql
@@ -94,5 +94,29 @@ BEGIN
 
 END$$
 
+DROP PROCEDURE IF EXISTS `addLevelsForFactor`$$
 
+CREATE PROCEDURE `addLevelsForFactor`(
+  IN p_labelid int
+  , IN p_values text
+  , IN p_experiment_id int)
+
+BEGIN
+
+  DECLARE stdvarid INT;
+  SELECT value INTO stdvarid FROM projectprop WHERE projectprop_id = p_labelid;
+
+  select IF(nd_experimentprop_id is NULL or min(nd_experimentprop_id) >= 0, -1, min(nd_experimentprop_id) -1)  
+                          INTO @newexpid 
+                          from nd_experimentprop;
+  
+    CALL split(@newexpid, p_experiment_id, p_values, '$%^');
+ 
+    insert into nd_experimentprop (nd_experimentprop_id, nd_experiment_id,type_id,value,rank) 
+    select id, levelno, stdvarid, `value`, 0
+    FROM temptbl;	
+
+
+
+END$$
 
--- IBFbScripts/src/scripts/Split.sql
+++ IBFbScripts/src/scripts/Split.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 
 DROP PROCEDURE if exists split$$
-CREATE PROCEDURE `split`(id_list varchar(2000), delimiter_str varchar(10))
+CREATE PROCEDURE `split`(genid int, p_levelno int, id_list text, delimiter_str varchar(10))
 BEGIN
 
   # routine that takes in a list of values delimited by delimeter_str, performs a split and returns the results
@@ -12,7 +12,7 @@ BEGIN
   declare done boolean default false;
 
   drop temporary table if exists temptbl;
-  create temporary table temptbl (id varchar(2000));
+  create temporary table temptbl (id int, levelno int, `value` varchar(2000));
 
 
   myloop: LOOP
@@ -26,17 +26,19 @@ BEGIN
 
     set id = (select substring(id_list,current_pos, next_pos-current_pos));
 
-    insert into temptbl values(trim(id));
+    insert into temptbl(id, levelno, `value`) values(genid, p_levelno, trim(id));
+    set genid = genid - 1;
+    set p_levelno = p_levelno - 1;
 
     if (done) then
 	   LEAVE myloop;
     end if;
 
-    set current_pos = next_pos+1;
+    set current_pos = next_pos+length(delimiter_str);
 
   end LOOP;
 
-  select * from temptbl;
+  -- select * from temptbl;
 
 END$$
 
